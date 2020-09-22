293ec55ec9e50e00ff0b5a358ff9e05cec7961f2
Stefan Russo <stefanrusso@mailbox.org>
2020-09-22 11:19:23 -0400

    Added MapLoaderDriver executable to .gitignore

--- src/MapLoader.h
+++ src/MapLoader.h
@@ -1,5 +1,31 @@
-#pragma once
-class MapLoader
-{
+#ifndef MAPLOADER_H
+#define MAPLOADER_H
+
+#include <string>
+#include <vector>
+
+class Continent {
+    int number;
+    string name;
+    int value;
+    string colour;
 };
+
+class Country {
+    int number;
+    string short_name;
+    int continent_number;
+    int x_coord;
+    int y_coord;
+    vector<int> borders;
+};
+
+class MapFile {
+    string pic_file_name;
+    string map_file_name;
+    string cards_file_name;
+    string prv_file_name;
+};
+
+#endif // MAPLOADER_H
 
