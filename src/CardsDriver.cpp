a6f65a8154e4a288f09496450f072a513f38fb92
Zahra Nikbakht <60522280+zahranikbakht@users.noreply.github.com>
2020-10-06 20:52:08 -0400

    Update CardsDriver.cpp

--- src/CardsDriver.cpp
+++ src/CardsDriver.cpp
@@ -7,21 +7,53 @@ int main() {
 
 	srand(time(NULL));
 
-	Deck d = Deck(10);
+	// create two decks and a hand
+	// deck 1 is randomized with 15 cards
+	// deck 2 is empty and can be manually filled for testing purposes
+	Deck d = Deck(15);
+	Deck d2 = Deck(0);
+
+	d2.add(BOMB);
+	d2.add(AIRLIFT);
+	d2.add(DIPLOMACY);
+	d2.add(BLOCKADE);
+	d2.add(REINFORCEMENT);
 
 	Hand h = Hand();
+
+	cout << "Deck 1 is: \n" << d << endl;
+
+	cout << "Drawing from Deck 1:\n" << endl;
 
 	for (int i = 0;i < 5;i++) {
 		d.draw(h);
 	}
 
-	cout << h <<endl;
-	
-	
+	cout << "My hand now is: " << h << endl;
+
+
 	for (int i = 0;i < 5;i++) {
 		h.returnByPos(0).Play(h, d);
 
 	}
-	
+
+
+	cout << "---------------------------\n" << endl;
+
+	cout << "Deck 2 is:\n" << d2 << endl;
+	cout << "Drawing from Deck 2:\n" << endl;
+
+	for (int i = 0;i < 5;i++) {
+		d2.draw(h);
+	}
+
+	cout << "My hand now is: " << h << endl;
+
+
+	for (int i = 0;i < 5;i++) {
+		h.returnByPos(0).Play(h, d2);
+
+	}
+
 	return 0;
 }
