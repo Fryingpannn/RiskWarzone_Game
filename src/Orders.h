6d021e6761d257ce7714e70c43463aa0766a3820
Matthew Pan <matthewdukepan@gmail.com>
2020-10-07 15:20:44 -0400

    test2

--- src/Orders.h
+++ src/Orders.h
@@ -1,8 +1,22 @@
 #pragma once
 
-//test
+#include <iostream>
+#include <string>
+
+//Order class
 class Orders
 {
-
+private:
+	string name;
+public:
+	boolean validate();
+	execute();
+	Order(Order copy);
+	Order& operator =();
+	friend ostream& operator <<(ostream& out, const Order &order);
 };
 
+class OrderList
+{
+	
+};
