5a62b2f803e0fbd8dfb86e0cf4c7955e45e5b6d0
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2020-10-11 09:01:20 -0400

    removed unnecesary comments and code

--- src/MapDriver.cpp
+++ src/MapDriver.cpp
@@ -9,21 +9,7 @@
 
 void mapDriver() 
 {
-    //// graph edges array.
-    //graphEdge edges[] = {
-    //    // (x, y, w) -> edge from x to y with weight w
-    //    {0,1,2, "Malaysia"},{0,2,4, "Malaysia"},{1,4,3, "Indonesia"},{2,3,2, "No idea"},{3,1,4, "Mars"},{4,3,3, "England"}
-    //};
-    //
-    //int Vertices = 6;      // Number of vertices in the graph
-    //// calculate number of edges
-    //int n = sizeof(edges) / sizeof(edges[0]);
-    //// construct graph
-    //Map diagraph(edges, n, Vertices);
-    //// print adjacency list representation of graph
-    //
-
-    //diagraph.Display();
+    
 
     //one way to optimise is to put enum
     //all these will be in stack memory
