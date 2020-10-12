97a5fff763b129ad3019c74856a7a6b7c3041426
Stefan Russo <stefansrusso@gmail.com>
2020-10-12 12:39:29 -0400

    Fixed stack overflow exceptions in Map.cpp, changed MapLoader to use new territory struct, updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,16 @@
 
 #include <iostream>
 #include <string>
-#include "MapDriver.hpp"
+#include "MapDriver.h"
 #include "MapLoaderDriver.hpp"
-// #define _CRTDBG_MAP_ALLOC
+#include "PlayerDriver.h"
+
 #include <stdlib.h>
-// #include <crtdbg.h>
 
 void displayMenu();
 
 int main()
 {
-    // _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     std::cout << "Assignment 1 - Demo" << std::endl;
     std::cout << "Main Menu" << std::endl;
     std::cout << "========================" << std::endl;
@@ -48,6 +47,7 @@ int main()
                 mapDriver();
                 break;
             case '3':
+                playerDriver();
                 break;
             case '4':
                 break;
@@ -72,31 +72,6 @@ void displayMenu()
 {
     std::cout << "1. MapLoader driver" << std::endl;
     std::cout << "2. Map driver" << std::endl;
+    std::cout << "3. Player driver" << std::endl;
 
 }
-
-
-//#include <SFML/Graphics.hpp>
-//
-//int main()
-//{
-//    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-//    sf::CircleShape shape(100.f);
-//    shape.setFillColor(sf::Color::Green);
-//
-//    while (window.isOpen())
-//    {
-//        sf::Event event;
-//        while (window.pollEvent(event))
-//        {
-//            if (event.type == sf::Event::Closed)
-//                window.close();
-//        }
-//
-//        window.clear();
-//        window.draw(shape);
-//        window.display();
-//    }
-//
-//    return 0;
-//}
